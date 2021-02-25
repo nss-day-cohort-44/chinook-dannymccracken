62fd045aa6ef070743065e62db13a93e72f5e103
Danny McCracken <dannymccracken13@yahoo.com>
2021-02-25 15:48:02 -0600

    completed up to 11

--- chinook.db.session.sql
+++ chinook.db.session.sql
@@ -1,3 +1,4 @@
+-- 1
 SELECT 
     CustomerId,
     FirstName,
@@ -6,6 +7,7 @@ SELECT
 FROM Customer
 WHERE Customer.Country != "USA";
 
+-- 2
 SELECT 
     CustomerId,
     FirstName,
@@ -14,6 +16,7 @@ SELECT
 FROM Customer
 WHERE Customer.Country = "Brazil";
 
+-- 3
 SELECT 
     c.FirstName,
     c.LastName,
@@ -25,15 +28,18 @@ JOIN Invoice i
             ON i.CustomerId = c.CustomerId
 WHERE c.Country = "Brazil";
 
+-- 4
 SELECT 
    *
 FROM Employee e
 WHERE e.Title = "Sales Support Agent";
 
+-- 5
 SELECT DISTINCT
     i.BillingCountry
 FROM Invoice i;
 
+-- 6
 SELECT 
     e.FirstName,
     e.LastName,
@@ -44,6 +50,7 @@ JOIN Customer c
 JOIN Invoice i
     ON i.CustomerId = c.CustomerId;
 
+-- 7
 SELECT 
     i.Total,
     c.FirstName AS customer_first,
@@ -56,3 +63,59 @@ JOIN Employee e
     ON c.SupportRepId = e.EmployeeId
 JOIN Invoice i
     ON i.CustomerId = c.CustomerId;
+
+-- 8
+SELECT 
+    COUNT()
+FROM Invoice i 
+
+-- 9 wrong
+SELECT SUM(total),
+    i.Total,
+    i.InvoiceDate
+FROM Invoice i 
+WHERE InvoiceDate LIKE "%2009%" OR InvoiceDate LIKE "%2011%";
+
+-- 10 
+SELECT 
+    COUNT(InvoiceLineId)
+FROM InvoiceLine 
+WHERE InvoiceId = 37 
+
+-- 11 wrong
+SELECT 
+    COUNT(InvoiceLineId)
+FROM InvoiceLine 
+GROUP BY InvoiceId
+
+-- 12
+
+-- 13
+
+-- 14
+
+-- 15
+
+-- 16
+
+-- 17
+
+-- 18
+
+-- 19
+
+-- 20
+
+-- 21
+
+-- 22
+
+-- 23
+
+-- 24
+
+-- 25
+
+-- 26
+
+-- 27
